34e7eaf4beca7b4aad2fb772774a77fa9177475d
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-11-11 17:40:27 -0500

    Empty IntelliJ file

---
+++ src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+
+public class InventoryManagementApplication{
+    public static void main(String[] args) {
+
+    }
+}
